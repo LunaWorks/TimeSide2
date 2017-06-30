a179d06cd5ddccbdc7c015473713210c9561c548
Balintskac <valentine_41@windowslive.com>
2017-06-30 17:25:12 +0200

    The new generate color was added to LuckyService class.

--- src/AppBundle/Service/LuckyService.php
+++ src/AppBundle/Service/LuckyService.php
@@ -8,4 +8,13 @@ class LuckyService
   public function generateLuckyNumber(){
       return rand(1,100);
   }
+
+  public function generateLuckyColor(){
+    $r = rand(128,255);
+      $g = rand(128,255);
+      $b = rand(128,255);
+      $color = dechex($r) . dechex($g) . dechex($b);
+      return "#".$color;
+  }
+
 }
