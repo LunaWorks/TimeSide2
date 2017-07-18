0cb9e87a91c00726db68b399e55b114ea83f660c
Balintskac <valentine_41@windowslive.com>
2017-07-18 13:17:49 +0200

    the process user input issue is working.

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,27 +1,30 @@
 <?php
+
 // src/AppBundle/Controller/LuckyController.php
+
 namespace AppBundle\Controller;
 
 use AppBundle\Service\LuckyService;
 use AppBundle\Model\UserViewModel;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-class LuckyController extends Controller
-{
+
+class LuckyController extends Controller {
+
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{name}")
      */
-    public function numberAction()
-    {
+    public function numberAction($name) {
         $service = new LuckyService();
         $model = new UserViewModel();
-        
+
         $model->number = $service->generateLuckyNumber();
         $model->color = $service->generateLuckyColor();
-  
-         return $this->render('lucky/number.html.twig', array(
-                 'model' => $model,
-            ));
-       
+        $model->name = $name;
+
+        return $this->render('lucky/number.html.twig', array(
+                    'model' => $model,
+        ));
     }
+
 }
