87646424ef31fa5f357fe74121bd43556d7e1220
Rápolthy Bálint <Rpolthy Blint@DESKTOP-0IAL8DC>
2017-12-09 22:36:59 +0100

    The timecontroller and view is finished.

--- src/TimeManagementBundle/Controller/TimeController.php
+++ src/TimeManagementBundle/Controller/TimeController.php
@@ -9,45 +9,45 @@ use \Datetime;
 
 class TimeController extends Controller
 {
-     /**
-     * $Path("/time/time.html.twig'")
-     */
     private $time;
     private $time2;
 
+    /**
+     * @Route("/time/start")
+     */
     public function start() {
+        $result = 0;
         $date = new DateTime();
-        return $this->render('time/time.html.twig', array('time' =>  $this->time = $date->getTimestamp()));
-    }
-
-    public function stop() {
-        $date2 = new DateTime();
-        return $this->render('time/time.html.twig', array('time2' =>  $this->time2 = $date2->getTimestamp()));
-    }
-
-    public function getResult() {
-        $service = new TimeService();
-        $result =  $service->formatDifference($time, $time2);
-        return $this->render('time/time.html.twig', array('result' => $result));
-    }
-}
-    
-
-       
-    /*
-    public function indexAction()
-    {
-         $service = new TimeService();
-         
-         $diff = $service->formatDateTime(SELF::getResult());
-         
         return $this->render('time/time.html.twig', array(
-
-                    'time' => SELF::start(),
-                    'time2' => SELF::stop(),
-                    'diff' => $diff,
+            'time' =>$this->time = $date->getTimestamp(), 
+            'time2' =>$this->time2 = $date->getTimestamp(),
+            'result' => $result    
         ));
     }
-      */
+    /**
+     * @Route("/time/stop")
+     */
+    public function stop() {
+        $result = 0;
+        $date2 = new DateTime();
+        return $this->render('time/time.html.twig', array(
+            'time' =>$this->time = $date->getTimestamp(), 
+            'time2' =>$this->time2 = $date->getTimestamp(),
+            'result' => $result    
+        ));
+    }
+    /**
+     * @Route("/time/split")
+     */
+    public function getResult() {
+        $service = new TimeService();
+        return $this->render('time/time.html.twig', array(
+            'time' =>$this->time, 
+            'time2' =>$this->time2,
+            'result' => $result = $service->calcDifference($this->time,$this->time2)
+        ));
+    }
+}
+
      
 
