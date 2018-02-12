174c8373283ce87d00016cb49c247150651db769
Mark Kalinovits <mkalinovits@chemaxon.com>
2018-02-12 12:28:16 +0100

    Using parameters.php for production.
    2

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,23 +1,19 @@
 <?php
 
-// Using ClearDB for mysql database
-$db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $db['host']);
-$container->setParameter('database_port', '~');
-$container->setParameter('database_name', trim($db['path'], '/'));
+$container->setParameter('database_port', $db['port']);
+$container->setParameter('database_name', substr($db["path"], 1));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
-
-// Email
-$container->setParameter('mailer_transport', 'smtp');
-$container->setParameter('mailer_host', '127.0.0.1');
+$container->setParameter('secret', 'ThisTokenIsNotSoSecretChangeIt'/*getenv('SECRET')*/);
+$container->setParameter('locale', 'en');
+$container->setParameter('mailer_transport', null);
+$container->setParameter('mailer_host', null);
 $container->setParameter('mailer_user', null);
 $container->setParameter('mailer_password', null);
-
-// Other settings
-$container->setParameter('locale', 'en');
-$container->setParameter('secret', 'ThisTokenIsNotSoSecretChangeIt');
 
 ?>
 
