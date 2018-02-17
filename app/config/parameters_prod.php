ff495f93c55bafecde76ba0ff8079bde39fe9e93
Mark Kalinovits <mkalinovits@chemaxon.com>
2018-02-17 21:40:12 +0100

    Updated production config

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -8,7 +8,7 @@ $container->setParameter('database_port', $db['port']);
 $container->setParameter('database_name', substr($db["path"], 1));
 $container->setParameter('database_user', $db['user']);
 $container->setParameter('database_password', $db['pass']);
-$container->setParameter('secret', 'ThisTokenIsNotSoSecretChangeIt'/*getenv('SECRET')*/);
+$container->setParameter('secret', getenv('SECRET'));
 $container->setParameter('locale', 'en');
 $container->setParameter('mailer_transport', null);
 $container->setParameter('mailer_host', null);
