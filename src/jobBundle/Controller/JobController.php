a17a43f0332c3bb044d5a83191153ff0d7d50a96
Rápolthy Bálint <Rpolthy Blint@DESKTOP-0IAL8DC>
2018-08-31 20:12:51 +0200

    All the operations work in Service class.

--- src/jobBundle/Controller/JobController.php
+++ src/jobBundle/Controller/JobController.php
@@ -7,54 +7,49 @@ use jobBundle\Service\JobService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
-    
 // For database operations
 use jobBundle\Entity\Entity\jobEntity;
 use Symfony\Component\HttpFoundation\Response;
-
 // Form types
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class JobController extends Controller
-{
+class JobController extends Controller {
+
     /**
      * @var jobService
      */
     private $jobService = null;
-    
+
     /**
      * JobController constructor.
      * @param JobService $service
      */
-    public function __construct(JobService $service)
-    {
+    public function __construct(JobService $service) {
         $this->jobService = $service;
     }
-    
-     /**
-      * 
-      * Thte main menu of the job site.
-      * 
+
+    /**
+     * 
+     * Thte main menu of the job site.
+     * 
      * @Route("/job")
      */
-    public function index()
-    {
-      return $this->render('job/job.html.twig');
+    public function index() {
+        return $this->render('job/job.html.twig');
     }
-    
+
     /**
      * 
      * Select everything from database ordered by the id.
      * 
      * @Route("job/job_select")
      */
-     public function getJobs()      
-     {
-      return $this->render('job/job_select.html.twig', array(
-            'jobs' => $this->jobService->getAllJobs()
-      ));
-     }
+    public function getJobs() {
+        return $this->render('job/job_select.html.twig', array(
+                    'jobs' => $this->jobService->getAllJobs()
+        ));
+    }
 
     /**
      * The user can add new job types to the database.
@@ -65,208 +60,195 @@ class JobController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
-    public function addJob(Request $request)
-    {
+    public function addJob(Request $request) {
         // Creating form
         $form = $this->formGeneration();
-        
+
         // Process request into form
         $form->handleRequest($request);
-        
+
         // Render templates depending on previous actions
-        if($this->formProcess($form)) {
+        if ($this->formProcess($form)) {
             $this->addFlash(
-               'notice',
-               'Job added'
+                    'notice', 'Job added'
             );
             return $this->redirectToRoute("job_job_getjobs");
         } else {
             return $this->render(
-                'job/job_new.html.twig', array(
-                'form' => $form->createView()
+                            'job/job_new.html.twig', array(
+                        'form' => $form->createView()
             ));
         }
     }
-    
+
     /**
      * @return Form $form
      */
-    public function formGeneration()
-    {
-       return $this->createFormBuilder()
-            ->add('name', TextType::class)
-            ->add('Felvesz', SubmitType::class)
-            ->getForm();
+    public function formGeneration() {
+        return $this->createFormBuilder()
+                        ->add('name', TextType::class)
+                        ->add('Felvesz', SubmitType::class)
+                        ->getForm();
     }
-    
+
     /**
      * @param Form $form
      * @return bool
      */
-    public function formProcess(Form $form)
-    {
+    public function formProcess($form) {
         if ($form->isSubmitted() && $form->isValid()) {
             // Do the business logic
             return $this->jobService->addJob($form);
-        }  
+        }
         return false;
     }
-     /**
+
+    /**
      * @param Form $form
      * @return bool
      */
-    public function formProcess2($form)
-    {
+    public function formProcess2($form) {
         if ($form->isSubmitted() && $form->isValid()) {
             // Do the business logic
             return $this->jobService->getJob($form);
-        }  
+        }
         return false;
     }
-      /**
+
+    /**
      * @param Form $form, jobEntity $jobs, int $job_id
      * @return bool
      */
-    public function formProcess3($form, $jobs,$job_id)
-    {
+    public function formProcess3($form, $jobs) {
         if ($form->isSubmitted() && $form->isValid()) {
             // Do the business logic
-             return $this->jobService->updateJobs($form,$jobs,$job_id);
-        }  
+            return $this->jobService->updateJob($form, $jobs);
+        }
         return false;
     }
-    
-     /**
-      * 
-      * Find a job by its id, if it is already in database.
-      * 
-      * @Route("job/job_search")
-      * @param Request $request
-      * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
-     */
-    
-    public function getJob(Request $request)
-    {
 
-       $form = $this->createFormBuilder()
-        ->add('name', TextType::class)
-        ->add('Keres', SubmitType::class)
-        ->getForm();
+    /**
+     * 
+     * Find a job by its id, if it is already in database.
+     * 
+     * @Route("job/job_search")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function getJob(Request $request) {
+        // Creating the form
+        $form = $this->createFormBuilder()
+                ->add('name', TextType::class)
+                ->add('Keres', SubmitType::class)
+                ->getForm();
         $form->handleRequest($request);
-        
-       if($this->formProcess2($form)) {
-            // Do the business logic
-          
-        // Render templates depending on previous actions
+
+        if ($this->formProcess2($form)) {
+            // Render templates depending on previous actions
             $this->addFlash(
-               'notice',
-               'Job found'
+                    'notice', 'Job found'
             );
             // Do the business logic
-         return new Response($this->jobService->getJobId($form));
-          
+            $jobid = $this->jobService->getJobId($form);
+            
+            // Check if this job exists
+            if ($jobid) {
+                return new Response("ID IS {$jobid[0]->getId()} FOR {$form['name']->getData()}");
+            } else {
+                return new Response("There is no result for {$form['name']->getData()}");
+            }
         } else {
-        return $this->render(
-            'job/job_search.html.twig', array(
-            'form' => $form->createView()
-         ));
+            return $this->render(
+                            'job/job_search.html.twig', array(
+                        'form' => $form->createView()
+            ));
         }
-    } 
-    
-      /**
-       * 
-       * Select all of the jobs, and select for update the name of the job.
-       * @Route("job/job_update")
-       * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
-     */
-    public function getJobsForUpdate()
-    {
-         return $this->render('job/job_update.html.twig', array(
-            'jobs' => $this->jobService->getAllJobs()
-          ));
     }
-       
-     /**
-      * 
-      * Update the job name.
-      * 
-      * @Route("job/job_update/{job_id}")
-     */
-    public function updateJob($job_id,Request $request)
-    {
-        if (!$this->jobService->Updatejob($job_id)) {
-            
-           return new Response('There is no such an ID '.$job_id );
-           
-        } else {
-            
-         $jobs = $this->getDoctrine()
-           ->getRepository(jobEntity::class)
-           ->find($job_id);
-         
-        $form = $this->createFormBuilder()
-           ->add('name', TextType::class)
-           ->add('Update', SubmitType::class)
-           ->getForm();
-  
-        $form['name']->setData($jobs->getName());
-        
-        $form->handleRequest($request);
- 
-            if($this->formProcess3($form,$jobs,$job_id)){
 
-               $this->addFlash(
-                  'notice',
-                  'Job found'
-               );
-                return $this->redirectToRoute("job_job_getjobs");
-           } 
-           else 
-           {
-                return $this->render('job/job_update_id.html.twig', array(
-                    'form' => $form->createView()
-                 ));
-           }
-        }
-    } 
-     /**
-       * 
-       * Select all of the jobs, and select for delete the job.
-       * @Route("job/job_delete")
-       * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
-     */
-    public function getJobsForDelete()
-    {
-         return $this->render('job/job_delete.html.twig', array(
-            'jobs' => $this->jobService->getAllJobs()
-          ));
-    }
-    
     /**
-      * 
-      * Delete the job from database
-      * 
-      * @Route("job/job_delete/{job_name}")
+     * 
+     * Select all of the jobs, and select for update the name of the job.
+     * @Route("job/job_update")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
-    public function deleteJob($job_name)
-    {
-        if (!$this->jobService->FindJobForDelete($job_name)) {
-            
-           return new Response('There is no such an id '.$job_name);
-           
-        } else {
-             $entityManager = $this->getDoctrine()->getManager();
-             $jobs =  $entityManager->getRepository(jobEntity::class)->find($job_name);
-             
-            if($this->jobService->deleteJob($jobs)){
+    public function getJobsForUpdate() {
+        return $this->render('job/job_update.html.twig', array(
+                    'jobs' => $this->jobService->getAllJobs()
+        ));
+    }
 
-               $this->addFlash(
-                  'notice',
-                  'Job found'
-               );
-               
-              return $this->redirectToRoute("job_job_getjobs");
+    /**
+     * 
+     * Update the job name.
+     * 
+     * @Route("job/job_update/{job_id}")
+     */
+    public function updateJob(Request $request, $job_id) {
+        // If the id doesn't exist in the database
+        if (!$this->jobService->getForUpdate($job_id)) {
+
+            return new Response('There is no such an ID ' . $job_id);
+        } else {
+            // Do the business logic
+            $jobs = $this->jobService->getJobName($job_id);
+
+            $form = $this->createFormBuilder()
+                    ->add('name', TextType::class)
+                    ->add('Update', SubmitType::class)
+                    ->getForm();
+            // Write out the name for updating
+            $form['name']->setData($jobs->getName());
+
+            $form->handleRequest($request);
+            // Do the business logic and check the form
+            if ($this->formProcess3($form, $jobs)) {
+
+                $this->addFlash(
+                        'notice', 'Job found'
+                );
+                return $this->redirectToRoute("job_job_getjobs");
+            } else {
+                return $this->render('job/job_update_id.html.twig', array(
+                            'form' => $form->createView()
+                ));
             }
         }
     }
-} 
+
+    /**
+     * 
+     * Select all of the jobs, and select for delete the job.
+     * 
+     * @Route("job/job_delete")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function getJobsForDelete() {
+        return $this->render('job/job_delete.html.twig', array(
+                    'jobs' => $this->jobService->getAllJobs()
+        ));
+    }
+
+    /**
+     * 
+     * Delete the job from database
+     * 
+     * @Route("job/job_delete/{job_id}")
+     */
+    public function deleteJob($job_id) {
+        if (!$this->jobService->FindJobForDelete($job_id)) {
+            // If the id doesn't exist in the database
+            return new Response('There is no such an id ' . $job_id);
+        } else {
+            // Do the business logic
+            if ($this->jobService->deleteJob($this->jobService->getJobName($job_id))) {
+
+                $this->addFlash(
+                        'notice', 'Job found'
+                );
+                // Render templates depending on previous actions
+                return $this->redirectToRoute("job_job_getjobs");
+            }
+        }
+    }
+
+}
--- src/jobBundle/Service/JobService.php
+++ src/jobBundle/Service/JobService.php
@@ -2,9 +2,8 @@
 
 namespace jobBundle\Service;
 
-
 // Form types
-use Symfony\Component\Form\Form;    
+use Symfony\Component\Form\Form;
 use Doctrine\ORM\EntityManagerInterface;
 use jobBundle\Entity\Entity\jobEntity;
 
@@ -14,31 +13,29 @@ class JobService {
      * @var EntityManagerInterface
      */
     private $em;
+
     /**
      * JobService constructor.
      *
      * @param EntityManagerInterface $em
      */
-    public function __construct(EntityManagerInterface $em)
-    {
+    public function __construct(EntityManagerInterface $em) {
         $this->em = $em;
     }
-    
-    public function getAllJobs() 
-    {          
-       return  $this->em
-           ->getRepository(jobEntity::class)
-            ->findAll();   
+
+    public function getAllJobs() {
+        return $this->em
+                        ->getRepository(jobEntity::class)
+                        ->findAll();
     }
 
     /**
      * @param Form $form
      * @return bool
      */
-    public function addJob(Form $form)
-    {
+    public function addJob(Form $form) {
         $success = false;
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $job = new jobEntity();
             $name = $form['name']->getData();
             $job->setName($name);
@@ -48,79 +45,87 @@ class JobService {
         }
         return $success;
     }
-    
-     /**
+
+    /**
      * @param Form $form
      * @return bool
      */
-    public function getJob(Form $form)
-    {
+    public function getJob(Form $form) {
         $success = false;
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->em->getRepository(jobEntity::class)->findBy(['name' => $form['name']->getData()]);
             $success = true;
         }
         return $success;
     }
-    
-     public function getJobId(Form $form)
-    {
-         $jobs = $this->em->getRepository(jobEntity::class)->findBy(['name' => $form['name']->getData()]);
-        if($jobs)
-        {
-           
-           return "id: "." name: ".$form['name']->getData();
-           
-        }
-        else 
-        {
-            return "no found for ".$form['name']->getData();
+
+    /**
+     * @param Form $form
+     * @return jobEntity
+     */
+    public function getJobId(Form $form) {
+        return $this->em->getRepository(jobEntity::class)->findBy(['name' => $form['name']->getData()]);
+    }
+
+    /**
+     * @param int $job_id
+     * @return jobEntity
+     */
+    public function getJobName($job_id) {
+        return $this->em->getRepository(jobEntity::class)->find($job_id);
+    }
+
+    /**
+     * @param int $job_id
+     * @return bool
+     */
+    public function getForUpdate($job_id) {
+        $jobs = $this->em
+                ->getRepository(jobEntity::class)
+                ->find($job_id);
+        if ($jobs) {
+            return true;
+        } else {
+            return false;
         }
     }
 
-    public function Updatejob($job_id)
-    {
-         $jobs = $this->em
-           ->getRepository(jobEntity::class)
-           ->find($job_id);
-         if($jobs){
-             return true;
-         } else {
-             return false;
-         }
-    }
-    
-     public function FindJobForDelete($name)
-    {
-         $jobs = $this->em->getRepository(jobEntity::class)->findBy(['id' => $name]);
-         if($jobs){
-             return true;
-         } else {
-             return false;
-         }
-    }
-    
-    public function updateJobs(Form $form, $jobs,$job_id)
-    {
+    /**
+     * @param Form $form , jobEntity $jobs
+     * @return bool
+     */
+    public function updateJob(Form $form, $jobs) {
         $success = false;
-        if($form->isSubmitted() && $form->isValid())
-        {
-            $jobs = $this->em
-                ->getRepository(jobEntity::class)
-                ->find($job_id);
-            
-            if($jobs){       
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            if ($jobs) {
                 $jobs->setName($form['name']->getData());
-                $this->em->flush(); 
+                $this->em->flush();
                 $success = true;
             }
         }
         return $success;
     }
-    
-    public function deleteJob($name)
-    {
-        $this->em->remove($name);
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function FindJobForDelete($name) {
+        $jobs = $this->em->getRepository(jobEntity::class)->findBy(['id' => $name]);
+        if ($jobs) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param jobEntity $job
+     * @return bool
+     */
+    public function deleteJob($job) {
+        $this->em->remove($job);
         $this->em->flush();
         return true;
     }
